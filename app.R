b29575c0f734323bb2a29bfaa8a6376b136ae078
Falguni Desai <falgunidesai@MacBook-Air.local>
2023-09-15 14:00:46 -0300

    added filter by search and select

--- app.R
+++ app.R
@@ -2,7 +2,7 @@
 # install.packages("js")
 
 #list of packages required
-list.of.packages <- c("networkD3","dplyr","tidyr","reshape2","tidyverse","js","webshot","stringr")
+list.of.packages <- c("networkD3","shinyWidgets","dplyr","tidyr","reshape2","tidyverse","js","webshot","stringr")
 
 #checking missing packages from list
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
@@ -12,7 +12,7 @@ if(length(new.packages)) install.packages(new.packages, dependencies = TRUE)
 
 
 library(shiny)
-
+library(shinyWidgets)
 library(networkD3)
 
 library(dplyr)
@@ -150,12 +150,29 @@ ui <- fluidPage(
       actionButton("selMetaCat","Select Metadata Categories"),
       selectInput("metaCat", "Metadata Categories", choices = c()),
       
-      checkboxInput("header", "Header", TRUE),
+      #checkboxInput("header", "Header", TRUE),
       
-      selectInput("filter", "Filtercollapse the dataframe", choices = c("Yes","No"), selected = "Yes"),
+      #selectInput("filter", "Filtercollapse the dataframe", choices = c("Yes","No"), selected = "Yes"),
       selectInput("level", "Taxonomy level to collapse",choices = c("Phylum","Class","Order","Family","Genera","Species",""), selected = "Genera"),
       
       #numericInput("contribThresh", "RPKM threshold filter",10),
+      actionButton("selTaxCat","Select Taxa Categories"),
+      pickerInput("TaxCat","Filter by Taxa", choices = c(),
+                  options = pickerOptions(
+                    actionsBox = TRUE,
+                    liveSearch = TRUE,
+                    liveSearchNormalize = TRUE
+                    ),
+                  multiple = T),
+      
+      actionButton("selFuncCat","Select Function Categories"),
+      pickerInput("FuncCat","Filter by Functions", choices = c(),
+                  options = pickerOptions(
+                    actionsBox = TRUE,
+                    liveSearch = TRUE,
+                    liveSearchNormalize = TRUE
+                  ),
+                  multiple = T),
       
       actionButton("updateThresh", "Update the Gene Contribution Threshold from data", 
                    style="color: #000000; white-space:normal;"),
@@ -169,7 +186,7 @@ ui <- fluidPage(
     # textOutput("taxlevel"),
     # textOutput("contrib_threshold"),
     # uiOutput("metCat"),  
-    sankeyNetworkOutput("SNNET", width = "auto", height = "1000px"),
+    sankeyNetworkOutput("SNNET", width = "2000px", height = "2000px"),
     #sankeyNetworkOutput("SNNET"),  
     actionButton("run","Display Plot"),
     downloadButton("downld", label = "Download Plot")
@@ -182,7 +199,7 @@ ui <- fluidPage(
 server <- function(session, input, output) {
   
   ## Read the stratified file in longform TSV
-  stratified_table_longform_data <- eventReactive(input$run,{
+  stratified_table_longform_data <- eventReactive(input$selTaxCat,{
     # input$file1 will be NULL initially. After the user selects
     # and uploads a file, it will be a data frame with 'name',
     # 'size', 'type', and 'datapath' columns. The 'datapath'
@@ -192,15 +209,15 @@ server <- function(session, input, output) {
     # read.csv(input$stratTableLFfile$datapath, header = input$header)
     file1 <- input$stratTableLFfile
     # if(is.null(file1)){return()}
-    req(input$stratTableLFfile, input$header, file.exists(input$stratTableLFfile$datapath))
-    read.table(file = file1$datapath, sep = '\t', header = input$header, fill = TRUE)
+    req(input$stratTableLFfile, file.exists(input$stratTableLFfile$datapath))
+    read.table(file = file1$datapath, sep = '\t', header = TRUE, fill = TRUE)
 
   })
 
   metadata_table_data <- eventReactive(input$selMetaCat,{
     file2 <- input$MetadataTablefile
-    req(input$MetadataTablefile, input$header, file.exists(input$MetadataTablefile$datapath))
-    read.table(file = file2$datapath, sep = '\t', header = input$header)
+    req(input$MetadataTablefile, file.exists(input$MetadataTablefile$datapath))
+    read.table(file = file2$datapath, sep = '\t', header = TRUE)
     })
   
   metadata_cat_update <- observeEvent(input$selMetaCat,{
@@ -212,6 +229,26 @@ server <- function(session, input, output) {
   
   #metadataDF <- reactive()
   metadata_category <- reactive({input$metaCat})
+  
+  Taxa_cat_update <- observeEvent(input$selTaxCat,{
+    dataDF <- stratified_table_longform_data()
+    message ("Taxa categories:",unique(dataDF$Genus))
+    updatePickerInput(session,"TaxCat",choices = c(as.character(sort(unique(dataDF$Genus)))))    
+    #return(metadataDF)    
+  })
+  
+  #metadataDF <- reactive()
+  Tax_category <- reactive({input$TaxCat})
+  
+  Func_cat_update <- observeEvent(input$selFuncCat,{
+    dataDF <- stratified_table_longform_data()
+    message ("Function categories:",unique(dataDF$Gene))
+    updatePickerInput(session,"FuncCat",choices = c(as.character(sort(unique(dataDF$Gene)))))    
+    #return(metadataDF)    
+  })
+  
+  #metadataDF <- reactive()
+  Func_category <- reactive({input$FuncCat})
   
   Threshold_Slider_update <- observeEvent(input$updateThresh, {
     inputTableDF <- stratified_table_longform_data()
@@ -236,7 +273,8 @@ server <- function(session, input, output) {
   #   selectInput("metaCat-dropdown", "Metadata Categories:", items)
   # })
   
-  filtered <- eventReactive(input$run,{input$filter})
+  #filtered <- eventReactive(input$run,{input$filter})
+  filtered <- TRUE
   taxlevel <- eventReactive(input$run,{input$level})
   #contrib_threshold <- eventReactive(input$run,{input$contribThresh})
   
@@ -245,9 +283,6 @@ server <- function(session, input, output) {
   #output$filtered <- renderText({paste0(input$filter)})
   # output$taxlevel <- renderText({paste0(input$level)})
   # output$contrib_threshold <- renderText({paste0(input$contribThresh)})
-  
-  
-  
   
 
   nodes_links_dfs_list <- eventReactive(input$run,{
@@ -258,15 +293,32 @@ server <- function(session, input, output) {
     # if (is.null(stratfified_table_longform)) return(NULL)
     message ("Input table dims:",dim(stratified_table_longform))
     
-    if (filtered() != TRUE && taxlevel() == ''){
-    message ("The value for Taxa level collapsing:",filtered())
+    if (filtered != TRUE && taxlevel() == ''){
+    message ("The value for Taxa level collapsing:",filtered)
     #stratified_table_longform_filtered <- subset(stratified_table_longform, Contribution > 0) # Filter out links with contributions == 0
     nodesDF_longform <- create_Nodes_DF(stratified_table_longform)
     linksDF_longform <- create_Links_DF(stratified_table_longform, nodesDF_longform)
    }else
      {
     message ("The value for Taxa level collapsing:",taxlevel())
-    stratified_table_longform_tax_collapsed <- collapseTableByTaxonomy(stratified_table_longform,taxlevel())
+    message ("Init table dims:",dim(stratified_table_longform))   
+    message ("Selected Taxa are:",Tax_category())
+    #Tax_search_regex <- paste0(Tax_category(),collapse = "|")
+    #message ("Selected Taxa search string:",Tax_search_regex)
+    
+    message ("Selected Funtions are:",Func_category())
+    #Func_search_regex <- paste0(Func_category(),collapse = "|")
+    #message ("Selected Function search string:",Func_search_regex)
+    
+    stratified_table_longform_TaxFiltered <- stratified_table_longform %>%
+            filter(Genus %in% Tax_category())
+    message ("Table after Tax keyword filter:",dim(stratified_table_longform_TaxFiltered))
+    
+    stratified_table_longform_TaxFiltered_FuncFiltered <- stratified_table_longform_TaxFiltered %>%
+         filter(Gene %in% Func_category())
+    message ("Table after Func keyword filter:",dim(stratified_table_longform_TaxFiltered_FuncFiltered))
+    
+    stratified_table_longform_tax_collapsed <- collapseTableByTaxonomy(stratified_table_longform_TaxFiltered_FuncFiltered,taxlevel())
     
     message ("The Sample Category for  collapsing:",metadata_category())
     stratified_table_longform_sample_collapsed <- collapseTableBySampleMetadata(stratified_table_longform_tax_collapsed,metadata_table_data(),metadata_category())
@@ -358,10 +410,10 @@ server <- function(session, input, output) {
         links
           .style("stroke-opacity", function(d1) {
               if(d1.Function === d.name){
-              return 0.5
+              return 0.8
               }
               else if(d1.Sample === d.name){
-              return 0.5
+              return 0.8
               }
               else{
               return 0.2
@@ -379,10 +431,10 @@ server <- function(session, input, output) {
     snNET(),
     '
       function(el, x) {
-      
+
       d3.selectAll(".node text")
       .attr("font-weight", "bold");
-      
+
       d3.select(el)
       .selectAll(".node text")
       .filter(d => d.Sample)
@@ -392,13 +444,13 @@ server <- function(session, input, output) {
       .selectAll(".node text")
       .filter(d => d.Function)
       .attr("x", 20)
-      .attr("text-anchor", "begin");  
+      .attr("text-anchor", "begin");
       d3.select(el)
       .selectAll(".node text")
       .filter(d => d.Taxa)
       .attr("x", -10)
       .attr("text-anchor", "end");
-      
+
       var nodes = d3.selectAll(".node");
       var links = d3.selectAll(".link");
       nodes.select("rect").style("cursor", "pointer");
@@ -411,16 +463,16 @@ server <- function(session, input, output) {
       nodes.on("contextmenu", clicked);
       }
       );
-      
+
       //nodes.on("mousedown", clicked);
       function clicked(d, i) {
         links
           .style("stroke-opacity", function(d1) {
               if(d1.Function === d.name){
-              return 0.5
+              return 0.8
               }
               else if(d1.Sample === d.name){
-              return 0.5
+              return 0.8
               }
               else{
               return 0.2
@@ -432,6 +484,11 @@ server <- function(session, input, output) {
   '
   )
   })
+  
+  # snnet_final <- eventReactive (output$downld,{
+  #   htmlwidgets::saveWidget(snNET(), "./sampCollapsed_sn.html")
+  #   })
+  # 
   
   # output$filtered <- reactive({(paste0(input$filter))})
   # output$taxlevel <- reactive({(paste0(input$level))})
@@ -452,7 +509,7 @@ server <- function(session, input, output) {
       message("In save, html file:", file)
       htmlwidgets::saveWidget(widget = snnet_final(), file = file)
     }else{
-        saveNetwork(snnet_final(), "./sampCollapsed_sn.html")
+        htmlwidgets::saveWidget(snnet_final(), "./sampCollapsed_sn.html")
         webshot("./sampCollapsed_sn.html",file)
     }
     # dev.off
